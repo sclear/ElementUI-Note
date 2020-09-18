75e33661ae684c977c0d3c9fc0d250881483ec3f
jiangchao <1392227049@qq.com>
2020-09-18 10:11:49 +0800

    fix: 😂comment

--- utils/scroll-into-view.js
+++ utils/scroll-into-view.js
@@ -6,7 +6,7 @@
  */
 function scrollIntoView(container, selected) {
   if (Vue.prototype.$isServer) return;
-  
+
   // 目标不存在
   if (!selected) {
     container.scrollTop = 0;
@@ -17,36 +17,27 @@ function scrollIntoView(container, selected) {
   const offsetParents = [];
   let pointer = selected.offsetParent;
   // 从目标节点向上遍历直到容器范围
+  // 存在  当前 ！== 目标   所属关系
   while (pointer && container !== pointer && container.contains(pointer)) {
     offsetParents.push(pointer);
     pointer = pointer.offsetParent;
   }
+  // top位置
   const top = selected.offsetTop + offsetParents.reduce((prev, curr) => (prev + curr.offsetTop), 0);
+  // bottom位置
   const bottom = top + selected.offsetHeight;
+  // viewTop
   const viewRectTop = container.scrollTop;
+  // viewBottom
   const viewRectBottom = viewRectTop + container.clientHeight;
-
+  // target卷入上方
   if (top < viewRectTop) {
     container.scrollTop = top;
-  } else if (bottom > viewRectBottom) {
+  } 
+  // target卷入下方
+  else if (bottom > viewRectBottom) {
     container.scrollTop = bottom - container.clientHeight;
   }
 }
 
-//   // 存在  当前 ！== 目标   所属关系
-//   while (pointer && container !== pointer && container.contains(pointer)) {
-//     offsetParents.push(pointer);
-//     pointer = pointer.offsetParent;
-//   }
-//   // 目标所处top值
-//   const top = selected.offsetTop + offsetParents.reduce((prev, curr) => (prev + curr.offsetTop), 0);
-//   // 当前底部位于容器顶部临界点
-//   const bottom = top + selected.offsetHeight;
-//   const viewRectTop = container.scrollTop;
-//   const viewRectBottom = viewRectTop + container.clientHeight;
 
-//   if (top < viewRectTop) {
-//     container.scrollTop = top;
-//   } else if (bottom > viewRectBottom) {
-//     container.scrollTop = bottom - container.clientHeight;
-//   }
